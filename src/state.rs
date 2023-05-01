da4a0a067a4ff89c17ebaa8923f56952d4f7c86b
Stephan Seitz <stephan.seitz@fau.de>
2023-05-01 21:38:31 +0200

    Just have one function for video session

--- src/state.rs
+++ src/state.rs
@@ -3,7 +3,7 @@ use std::sync::RwLock;
 use ash::vk;
 use once_cell::sync::Lazy;
 
-use crate::settings::Settings;
+use {Codec, Settings};
 
 use crate::vk_beta::{
     VK_STD_VULKAN_VIDEO_CODEC_H264_DECODE_EXTENSION_NAME,
@@ -37,28 +37,47 @@ pub fn get_state() -> &'static State {
 }
 
 impl State {
-    pub fn create_encode_session(&self, queue_family_idx: u32, max_coded_extent: vk::Extent2D) {
+    pub fn create_video_session(
+        &self,
+        queue_family_idx: u32,
+        max_coded_extent: vk::Extent2D,
+        is_encode: bool,
+    ) {
         let profile = vk::VideoProfileInfoKHR::default()
-            .video_codec_operation(match self.settings.codec {
-                crate::settings::Codec::H264 => vk::VideoCodecOperationFlagsKHR::ENCODE_H264_EXT,
-                crate::settings::Codec::H265 => vk::VideoCodecOperationFlagsKHR::ENCODE_H265_EXT,
-                crate::settings::Codec::AV1 => todo!(),
+            .video_codec_operation(match (is_encode, self.settings.codec) {
+                (true, Codec::H264) => vk::VideoCodecOperationFlagsKHR::ENCODE_H264_EXT,
+                (true, Codec::H265) => vk::VideoCodecOperationFlagsKHR::ENCODE_H265_EXT,
+                (true, Codec::AV1) => todo!(),
+                (false, Codec::H264) => vk::VideoCodecOperationFlagsKHR::DECODE_H264,
+                (false, Codec::H265) => vk::VideoCodecOperationFlagsKHR::DECODE_H265,
+                (false, Codec::AV1) => todo!(),
             })
             .luma_bit_depth(vk::VideoComponentBitDepthFlagsKHR::TYPE_8)
             .chroma_bit_depth(vk::VideoComponentBitDepthFlagsKHR::TYPE_8)
             .chroma_subsampling(vk::VideoChromaSubsamplingFlagsKHR::TYPE_420);
-        let header_version = match self.settings.codec {
-            crate::settings::Codec::H264 => vk::ExtensionProperties::default()
+        let header_version = match (is_encode, self.settings.codec) {
+            (true, Codec::H264) => vk::ExtensionProperties::default()
                 .extension_name(unsafe {
                     *(VK_STD_VULKAN_VIDEO_CODEC_H264_ENCODE_EXTENSION_NAME.as_ptr() as *const _)
                 })
                 .spec_version(vk::make_api_version(0, 0, 9, 8)),
-            crate::settings::Codec::H265 => vk::ExtensionProperties::default()
+            (true, Codec::H265) => vk::ExtensionProperties::default()
                 .extension_name(unsafe {
                     *(VK_STD_VULKAN_VIDEO_CODEC_H265_ENCODE_EXTENSION_NAME.as_ptr() as *const _)
                 })
                 .spec_version(vk::make_api_version(0, 0, 9, 9)),
-            crate::settings::Codec::AV1 => todo!(),
+            (true, Codec::AV1) => todo!(),
+            (false, Codec::H264) => vk::ExtensionProperties::default()
+                .extension_name(unsafe {
+                    *(VK_STD_VULKAN_VIDEO_CODEC_H264_DECODE_EXTENSION_NAME.as_ptr() as *const _)
+                })
+                .spec_version(vk::make_api_version(0, 1, 0, 0)),
+            (false, Codec::H265) => vk::ExtensionProperties::default()
+                .extension_name(unsafe {
+                    *(VK_STD_VULKAN_VIDEO_CODEC_H265_DECODE_EXTENSION_NAME.as_ptr() as *const _)
+                })
+                .spec_version(vk::make_api_version(0, 1, 0, 0)),
+            (false, Codec::AV1) => todo!(),
         };
         let info = vk::VideoSessionCreateInfoKHR::default()
             .queue_family_index(queue_family_idx)
@@ -73,60 +92,30 @@ impl State {
             .video_usage_hints(vk::VideoEncodeUsageFlagsKHR::RECORDING)
             .video_content_hints(vk::VideoEncodeContentFlagsKHR::RENDERED)
             .tuning_mode(vk::VideoEncodeTuningModeKHR::HIGH_QUALITY);
-        profile.push_next(&mut encode_usage);
-        let mut h264_encode_profile = vk::VideoDecodeH264ProfileInfoKHR::default();
-        let mut h265_encode_profile = vk::VideoDecodeH264ProfileInfoKHR::default();
-        profile.push_next(match self.settings.codec {
-            crate::settings::Codec::H264 => &mut h264_encode_profile,
-            crate::settings::Codec::H265 => &mut h265_encode_profile,
-            crate::settings::Codec::AV1 => todo!(),
-        });
-        info.video_profile(&profile);
-    }
-
-    pub fn create_decode_session(&self, queue_family_idx: u32, max_coded_extent: vk::Extent2D) {
-        let profile = vk::VideoProfileInfoKHR::default()
-            .video_codec_operation(match self.settings.codec {
-                crate::settings::Codec::H264 => vk::VideoCodecOperationFlagsKHR::DECODE_H264,
-                crate::settings::Codec::H265 => vk::VideoCodecOperationFlagsKHR::DECODE_H265,
-                crate::settings::Codec::AV1 => todo!(),
-            })
-            .luma_bit_depth(vk::VideoComponentBitDepthFlagsKHR::TYPE_8)
-            .chroma_bit_depth(vk::VideoComponentBitDepthFlagsKHR::TYPE_8)
-            .chroma_subsampling(vk::VideoChromaSubsamplingFlagsKHR::TYPE_420);
-        let header_version = match self.settings.codec {
-            crate::settings::Codec::H264 => vk::ExtensionProperties::default()
-                .extension_name(unsafe {
-                    *(VK_STD_VULKAN_VIDEO_CODEC_H264_DECODE_EXTENSION_NAME.as_ptr() as *const _)
-                })
-                .spec_version(vk::make_api_version(0, 1, 0, 0)),
-            crate::settings::Codec::H265 => vk::ExtensionProperties::default()
-                .extension_name(unsafe {
-                    *(VK_STD_VULKAN_VIDEO_CODEC_H265_DECODE_EXTENSION_NAME.as_ptr() as *const _)
-                })
-                .spec_version(vk::make_api_version(0, 1, 0, 0)),
-            crate::settings::Codec::AV1 => todo!(),
-        };
-        let info = vk::VideoSessionCreateInfoKHR::default()
-            .queue_family_index(queue_family_idx)
-            .max_coded_extent(max_coded_extent)
-            .picture_format(vk::Format::G8_B8R8_2PLANE_420_UNORM)
-            .reference_picture_format(vk::Format::G8_B8R8_2PLANE_420_UNORM)
-            .max_dpb_slots(16)
-            .max_active_reference_pictures(0)
-            .std_header_version(&header_version);
-
-        let mut decode_usage = vk::VideoDecodeUsageInfoKHR::default()
-            .video_usage_hints(vk::VideoDecodeUsageFlagsKHR::STREAMING);
-        profile.push_next(&mut decode_usage);
-        info.video_profile(&profile);
+        let mut decode_usage = vk::VideoEncodeUsageInfoKHR::default()
+            .video_usage_hints(vk::VideoEncodeUsageFlagsKHR::STREAMING);
+        if is_encode {
+            profile.push_next(&mut encode_usage);
+        } else {
+            profile.push_next(&mut decode_usage);
+        }
+        let mut h264_encode_profile = vk::VideoEncodeH264ProfileInfoEXT::default();
+        let mut h265_encode_profile = vk::VideoEncodeH264ProfileInfoEXT::default();
         let mut h264_decode_profile = vk::VideoDecodeH264ProfileInfoKHR::default();
         let mut h265_decode_profile = vk::VideoDecodeH264ProfileInfoKHR::default();
-        profile.push_next(match self.settings.codec {
-            crate::settings::Codec::H264 => &mut h264_decode_profile,
-            crate::settings::Codec::H265 => &mut h265_decode_profile,
-            crate::settings::Codec::AV1 => todo!(),
-        });
+        if is_encode {
+            profile.push_next(match self.settings.codec {
+                Codec::H264 => &mut h264_encode_profile,
+                Codec::H265 => &mut h265_encode_profile,
+                Codec::AV1 => todo!(),
+            });
+        } else {
+            profile.push_next(match self.settings.codec {
+                Codec::H264 => &mut h264_decode_profile,
+                Codec::H265 => &mut h265_decode_profile,
+                Codec::AV1 => todo!(),
+            });
+        }
         info.video_profile(&profile);
     }
 }
