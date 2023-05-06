6b21242c45adb14168aa3f5060d30590b6dd61d5
Stephan Seitz <stephan.seitz@fau.de>
2023-05-07 01:23:33 +0200

    Remove unneeded cast to float3

--- shaders/bgr_to_uv.hlsl
+++ shaders/bgr_to_uv.hlsl
@@ -8,6 +8,6 @@ RWTexture2D<float2> uv;
 [numthreads(8, 8, 1)]
 void main( uint3 id : SV_DispatchThreadID ) {
     // Rec. 709 https://en.wikipedia.org/wiki/YCbCr
-    uv[id.xy] = mul(float2x3(float3(-0.1146 , -0.3854 ,  0.5) , float3(0.5, -0.4542 , -0.0458)), float3(rgba.SampleLevel(s, id.xy / 2, 0).rgb));
+    uv[id.xy] = mul(float2x3(float3(-0.1146 , -0.3854 ,  0.5) , float3(0.5, -0.4542 , -0.0458)), rgba.SampleLevel(s, id.xy / 2, 0).rgb);
 }
 
