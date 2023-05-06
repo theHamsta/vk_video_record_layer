3043db7b8306fbaefe9dd57abefe7111e014a74c
Stephan Seitz <stephan.seitz@fau.de>
2023-05-07 01:19:25 +0200

    Update shaders

--- shaders/bgr_to_uv.hlsl
+++ shaders/bgr_to_uv.hlsl
@@ -3,11 +3,11 @@ Texture2D<float4> rgba;
 [[vk::binding(1)]]
 SamplerState s;
 [[vk::binding(2)]]
-RWTexture2D<float> y;
+RWTexture2D<float2> uv;
 
 [numthreads(1, 1, 32)]
 void main( uint3 id : SV_DispatchThreadID ) {
     // Rec. 709 https://en.wikipedia.org/wiki/YCbCr
-    y[id.xy] = dot(float3(0.2126, 0.7152, 0.0722), rgba.SampleLevel(s, id.xy, 0).rgb);
+    uv[id.xy] = mul(float2x3(float3(-0.1146 , -0.3854 ,  0.5) , float3(0.5, -0.4542 , -0.0458)), float3(rgba.SampleLevel(s, id.xy / 2, 0).rgb));
 }
 
