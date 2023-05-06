c5a11fe8a92d3f51351d0597ca49820f25bcd60c
Stephan Seitz <stephan.seitz@fau.de>
2023-05-07 01:27:05 +0200

    Clang-format shaders

--- shaders/bgr_to_uv.hlsl
+++ shaders/bgr_to_uv.hlsl
@@ -1,13 +1,11 @@
-[[vk::binding(0)]]
-Texture2D<float4> rgba;
-[[vk::binding(1)]]
-SamplerState s;
-[[vk::binding(2)]]
-RWTexture2D<float2> uv;
+[[vk::binding(0)]] Texture2D<float4> rgba;
+[[vk::binding(1)]] SamplerState s;
+[[vk::binding(2)]] RWTexture2D<float2> uv;
 
 [numthreads(8, 8, 1)]
-void main( uint3 id : SV_DispatchThreadID ) {
-    // Rec. 709 https://en.wikipedia.org/wiki/YCbCr
-    uv[id.xy] = mul(float2x3(float3(-0.1146 , -0.3854 ,  0.5) , float3(0.5, -0.4542 , -0.0458)), rgba.SampleLevel(s, id.xy / 2, 0).rgb);
+void main(uint3 id : SV_DispatchThreadID) {
+  // Rec. 709 https://en.wikipedia.org/wiki/YCbCr
+  uv[id.xy] = mul(
+      float2x3(float3(-0.1146, -0.3854, 0.5), float3(0.5, -0.4542, -0.0458)),
+      rgba.SampleLevel(s, id.xy * 2, 0).rgb);
 }
-
