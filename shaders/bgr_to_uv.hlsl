6d60ff20756d72a6f100fa38953a68be67bfb2d2
Stephan Seitz <stephan.seitz@fau.de>
2023-05-07 02:43:14 +0200

    Actually subsample by calculating mean

--- shaders/bgr_to_uv.hlsl
+++ shaders/bgr_to_uv.hlsl
@@ -3,9 +3,13 @@
 [[vk::binding(2)]] RWTexture2D<float2> uv;
 
 [numthreads(8, 8, 1)]
-void main(uint3 id : SV_DispatchThreadID) {
+void main(uint3 id: SV_DispatchThreadID) {
+  float3 mean = 0.25 * (rgba.SampleLevel(s, id.xy * 2, 0).rgb +
+                        rgba.SampleLevel(s, id.xy * 2 + int2(1, 0), 0).rgb +
+                        rgba.SampleLevel(s, id.xy * 2 + int2(0, 1), 0).rgb +
+                        rgba.SampleLevel(s, id.xy * 2 + int2(1, 1), 0).rgb);
   // Rec. 709 https://en.wikipedia.org/wiki/YCbCr
   uv[id.xy] = mul(
       float2x3(float3(-0.1146, -0.3854, 0.5), float3(0.5, -0.4542, -0.0458)),
-      rgba.SampleLevel(s, id.xy * 2, 0).rgb);
+      mean);
 }
